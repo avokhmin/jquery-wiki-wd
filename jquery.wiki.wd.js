8f42256239dac679b57cfad280c457c42da840b0
Vokhmin Alexey V <avokhmin@gmail.com>
2012-06-25 20:37:27 +0400

    removed unnecessary line

--- jquery.wiki.wd.js
+++ jquery.wiki.wd.js
@@ -1,7 +1,6 @@
 /******************************************
  *
  * @author          Vokhmin Alexey V
- * @copyright       Copyright (c) 2012 Websanova.
  * @license         This wiki-wd jQuery plug-in is dual licensed under the MIT and GPL licenses.
  * @link            https://github.com/avokhmin/jquery-wiki-wd
  * @version         Version 0.1
